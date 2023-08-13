9427859a74f723b323ca58f17a9bba82f6d70157
キジトラニャン/お魚咥えなかった世界線 <83628516+osakanakuwaenai@users.noreply.github.com>
2023-08-13 17:18:22 +0900

    Create v.0.0.1's first file

---
+++ v0_0_1/index.js
@@ -0,0 +1,33 @@
+class JSCPElement extends HTMLElement{
+  constructor(){
+    super();
+    this._src = null;
+  }
+
+  // Its observed attribute is only "src".
+  static get observedAttributes() {
+    return ["src"];
+  }
+
+  // callback when its attr was changed
+  attributeChangedCallback(src, oldValue, newValue) {
+    this._src = newValue;
+    this._updateRendering();
+  }
+
+  connectedCallback() {
+    this._updateRendering();
+  }
+
+  get src() {
+    return this._name;
+  }
+
+  set src(v) {
+    this._updateRendering();
+    this.setAttribute("src", v);
+  }
+  
+}
+
+customElements.define("jscp-ld",JSCPElement);//JSCP LoaD
